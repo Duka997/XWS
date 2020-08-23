ee63c962d77c43f452566f75aee142d7296b38b0
miilica <milicaradovanovic64@gmail.com>
2020-08-23 21:17:00 +0200

    Create KlasaAutomobilaService.java

---
+++ demo/src/main/java/com/demo/service/KlasaAutomobilaService.java
@@ -0,0 +1,66 @@
+package com.demo.service;
+
+import com.demo.dto.KlasaAutomobilaDTO;
+import com.demo.model.KlasaAutomobila;
+import com.demo.repository.KlasaAutomobilaRepository;
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.AccessDeniedException;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+public class KlasaAutomobilaService {
+
+    @Autowired
+    private KlasaAutomobilaRepository klasaAutoRepository;
+
+    @Autowired
+    private ModelMapper modelMapper;
+
+    public KlasaAutomobila findById(Long id) throws AccessDeniedException {
+        KlasaAutomobila u = klasaAutoRepository.findById(id).orElseGet(null);
+        return u;
+    }
+
+
+    public KlasaAutomobila save(KlasaAutomobilaDTO mDTO) {
+        KlasaAutomobila m = new KlasaAutomobila();
+        m.setNaziv(mDTO.getNaziv());
+        m.setObrisan(false);
+        m = this.klasaAutoRepository.save(m);
+        return m;
+    }
+
+    public ResponseEntity<?> findAll() {
+
+        List<KlasaAutomobila> result = klasaAutoRepository.findAll();
+        List<KlasaAutomobilaDTO> klasaAutomobilaDTOS = new ArrayList<>();
+
+        for(KlasaAutomobila ma: result){
+            if(!ma.isObrisan()) {
+                klasaAutomobilaDTOS.add(modelMapper.map(ma, KlasaAutomobilaDTO.class));
+            }
+        }
+
+        return new ResponseEntity<>(klasaAutomobilaDTOS, HttpStatus.OK);
+    }
+    public KlasaAutomobila edit(KlasaAutomobilaDTO klasaAutomobilaDTO) throws java.nio.file.AccessDeniedException {
+        KlasaAutomobila klasaAutomobila = this.findById(klasaAutomobilaDTO.getId());
+        klasaAutomobila.setId(klasaAutomobilaDTO.getId());
+        klasaAutomobila.setNaziv(klasaAutomobilaDTO.getNaziv());
+        this.klasaAutoRepository.save(klasaAutomobila);
+        return klasaAutomobila;
+    }
+
+    public void delete(Long id) throws java.nio.file.AccessDeniedException {
+        KlasaAutomobila klasaAutomobila = this.findById(id);
+        klasaAutomobila.setObrisan(true);
+        this.klasaAutoRepository.save(klasaAutomobila);
+    }
+
+}

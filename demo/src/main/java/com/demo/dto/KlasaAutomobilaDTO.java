ff2fe1a4de9734c593d523c5e1a0c446bc36d5e0
miilica <milicaradovanovic64@gmail.com>
2020-08-23 21:16:57 +0200

    Create KlasaAutomobilaDTO.java

---
+++ demo/src/main/java/com/demo/dto/KlasaAutomobilaDTO.java
@@ -0,0 +1,34 @@
+package com.demo.dto;
+
+public class KlasaAutomobilaDTO {
+    private Long id;
+    private String naziv;
+    private boolean obrisan;
+
+    public KlasaAutomobilaDTO() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNaziv() {
+        return naziv;
+    }
+
+    public void setNaziv(String naziv) {
+        this.naziv = naziv;
+    }
+
+    public boolean isObrisan() {
+        return obrisan;
+    }
+
+    public void setObrisan(boolean obrisan) {
+        this.obrisan = obrisan;
+    }
+}

f05e3e30e1d5388e46ef319ee6c3bac71d8f8c4a
miilica <milicaradovanovic64@gmail.com>
2020-08-23 22:20:53 +0200

    Update TipMjenjaca.java

--- demo/src/main/java/com/demo/model/TipMjenjaca.java
+++ demo/src/main/java/com/demo/model/TipMjenjaca.java
@@ -10,10 +10,7 @@ import javax.persistence.*;
 import java.util.Set;
 
 @Entity
-@Getter
-@Setter
 @AllArgsConstructor
-@NoArgsConstructor
 @Table(name = "tipmjenjaca")
 public class TipMjenjaca {
 
@@ -23,4 +20,34 @@ public class TipMjenjaca {
 
         @Column
         private String naziv;
+
+        @Column
+        private boolean obrisan;
+
+        public TipMjenjaca() {
+        }
+
+        public Long getId() {
+                return id;
+        }
+
+        public void setId(Long id) {
+                this.id = id;
+        }
+
+        public String getNaziv() {
+                return naziv;
+        }
+
+        public void setNaziv(String naziv) {
+                this.naziv = naziv;
+        }
+
+        public boolean isObrisan() {
+                return obrisan;
+        }
+
+        public void setObrisan(boolean obrisan) {
+                this.obrisan = obrisan;
+        }
 }

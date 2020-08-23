896ea074f86841e059d076fabd00f7a9b99b81cc
miilica <milicaradovanovic64@gmail.com>
2020-08-23 22:20:31 +0200

    Create TipMjenjacaDTO.java

---
+++ demo/src/main/java/com/demo/dto/TipMjenjacaDTO.java
@@ -0,0 +1,35 @@
+package com.demo.dto;
+
+public class TipMjenjacaDTO {
+
+    private Long id;
+    private String naziv;
+    private boolean obrisan;
+
+    public TipMjenjacaDTO() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNaziv() {
+        return naziv;
+    }
+
+    public void setNaziv(String naziv) {
+        this.naziv = naziv;
+    }
+
+    public boolean isObrisan() {
+        return obrisan;
+    }
+
+    public void setObrisan(boolean obrisan) {
+        this.obrisan = obrisan;
+    }
+}

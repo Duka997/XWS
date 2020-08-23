a4734d704ed92f858c43703d10dfb20590fc7513
miilica <milicaradovanovic64@gmail.com>
2020-08-23 17:55:45 +0200

    Create addMarkuAutomobila.component.ts

---
+++ appFrontend/src/app/components/addMarkuAutomobila/addMarkuAutomobila.component.ts
@@ -0,0 +1,33 @@
+import { Component, OnInit } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { Router } from '@angular/router';
+import { DodajMarkuAutomobilaService } from './addMarkuAutomobila.service';
+import { MarkaAutomobila } from 'src/app/model/markaAutomobila';
+
+
+@Component({
+  selector: 'app-addMarkuAutomobila',
+  templateUrl: './addMarkuAutomobila.component.html'
+})
+export class AddMarkaAutomobila implements OnInit {
+
+ marka =  new MarkaAutomobila(null,null,null);
+  constructor(private _markaServis : DodajMarkuAutomobilaService, private router : Router) { }
+
+  ngOnInit(): void {
+
+  }
+  onSubmit(){ 
+    console.log(this.marka);
+      this._markaServis.dodajMarku(this.marka)
+     .subscribe(
+         data=>{
+          console.log('Success!', JSON.stringify(data))
+          alert('Nova marka dodata!');
+         // this.router.navigate(['admin']);
+         } ,
+          error=> console.error('Error!',error)
+      )
+  }
+
+}

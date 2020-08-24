b60fdde069a86bf96bfdb8fad0a0f15d944eec89
miilica <milicaradovanovic64@gmail.com>
2020-08-24 15:53:23 +0200

    Create VoziloRepository.java

---
+++ demo/src/main/java/com/demo/repository/VoziloRepository.java
@@ -0,0 +1,13 @@
+package com.demo.repository;
+
+import com.demo.model.Vozilo;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+@EnableJpaRepositories
+public interface VoziloRepository  extends JpaRepository<Vozilo, Long> {
+}

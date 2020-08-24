bf8f0e152ccfe8e4148f15baa4c0826d3011a8a3
Katarina Prodanovic <57413828+Katarina1910@users.noreply.github.com>
2020-08-24 12:56:17 +0200

    Backend for manipulation with users

--- demo/src/main/java/com/demo/repository/UserRepository.java
+++ demo/src/main/java/com/demo/repository/UserRepository.java
@@ -11,4 +11,6 @@ import java.util.List;
 public interface UserRepository extends JpaRepository<User, Long> {
     User findByUsername(String username);
     List<User> findByLastPasswordResetDate(Timestamp lastPasswordResetDate);
+
+    List<User> findAllByDeleted(boolean b);
 }

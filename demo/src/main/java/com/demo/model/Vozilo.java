2535ae2bdb6f1ad070bc0f71b5a336cdc4ef9841
miilica <milicaradovanovic64@gmail.com>
2020-08-24 15:53:34 +0200

    Update Vozilo.java

--- demo/src/main/java/com/demo/model/Vozilo.java
+++ demo/src/main/java/com/demo/model/Vozilo.java
@@ -9,10 +9,7 @@ import javax.persistence.*;
 import java.util.Set;
 
 @Entity
-@NoArgsConstructor
 @AllArgsConstructor
-@Getter
-@Setter
 @Table(name = "vozilo")
 public class Vozilo {
 
@@ -72,4 +69,154 @@ public class Vozilo {
 
     @OneToMany(mappedBy = "vozilo")
     private Set<Oglas> oglasi;
+
+    @OneToMany(mappedBy = "vozilo", cascade = CascadeType.ALL)
+    private Set<Slika> slike;
+
+    public Vozilo() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public double getCijena() {
+        return cijena;
+    }
+
+    public void setCijena(double cijena) {
+        this.cijena = cijena;
+    }
+
+    public double getKilometraza() {
+        return kilometraza;
+    }
+
+    public void setKilometraza(double kilometraza) {
+        this.kilometraza = kilometraza;
+    }
+
+    public double getMozePreciKM() {
+        return mozePreciKM;
+    }
+
+    public void setMozePreciKM(double mozePreciKM) {
+        this.mozePreciKM = mozePreciKM;
+    }
+
+    public boolean isImaAndroid() {
+        return imaAndroid;
+    }
+
+    public void setImaAndroid(boolean imaAndroid) {
+        this.imaAndroid = imaAndroid;
+    }
+
+    public boolean isColiisionDamageWavier() {
+        return coliisionDamageWavier;
+    }
+
+    public void setColiisionDamageWavier(boolean coliisionDamageWavier) {
+        this.coliisionDamageWavier = coliisionDamageWavier;
+    }
+
+    public int getBrSjedistaZaDjecu() {
+        return brSjedistaZaDjecu;
+    }
+
+    public void setBrSjedistaZaDjecu(int brSjedistaZaDjecu) {
+        this.brSjedistaZaDjecu = brSjedistaZaDjecu;
+    }
+
+    public double getOcjena() {
+        return ocjena;
+    }
+
+    public void setOcjena(double ocjena) {
+        this.ocjena = ocjena;
+    }
+
+    public MarkaAutomobila getMarkaAutomobila() {
+        return markaAutomobila;
+    }
+
+    public void setMarkaAutomobila(MarkaAutomobila markaAutomobila) {
+        this.markaAutomobila = markaAutomobila;
+    }
+
+    public KlasaAutomobila getKlasaAutomobila() {
+        return klasaAutomobila;
+    }
+
+    public void setKlasaAutomobila(KlasaAutomobila klasaAutomobila) {
+        this.klasaAutomobila = klasaAutomobila;
+    }
+
+    public TipGoriva getTipGoriva() {
+        return tipGoriva;
+    }
+
+    public void setTipGoriva(TipGoriva tipGoriva) {
+        this.tipGoriva = tipGoriva;
+    }
+
+    public TipMjenjaca getTipMjenjaca() {
+        return tipMjenjaca;
+    }
+
+    public void setTipMjenjaca(TipMjenjaca tipMjenjaca) {
+        this.tipMjenjaca = tipMjenjaca;
+    }
+
+    public Set<Izvjestaj> getIzvjestaji() {
+        return izvjestaji;
+    }
+
+    public void setIzvjestaji(Set<Izvjestaj> izvjestaji) {
+        this.izvjestaji = izvjestaji;
+    }
+
+    public Set<Komentar> getKomentari() {
+        return komentari;
+    }
+
+    public void setKomentari(Set<Komentar> komentari) {
+        this.komentari = komentari;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    public Set<Ocjena> getOcjene() {
+        return ocjene;
+    }
+
+    public void setOcjene(Set<Ocjena> ocjene) {
+        this.ocjene = ocjene;
+    }
+
+    public Set<Oglas> getOglasi() {
+        return oglasi;
+    }
+
+    public void setOglasi(Set<Oglas> oglasi) {
+        this.oglasi = oglasi;
+    }
+
+    public Set<Slika> getSlike() {
+        return slike;
+    }
+
+    public void setSlike(Set<Slika> slike) {
+        this.slike = slike;
+    }
 }

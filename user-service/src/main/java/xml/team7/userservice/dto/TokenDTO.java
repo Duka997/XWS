6137b46f9477079c8eabaaff150cc8962463c6f4
Katarina Prodanovic <57413828+Katarina1910@users.noreply.github.com>
2020-08-31 08:44:38 +0200

    added dtos

---
+++ user-service/src/main/java/xml/team7/userservice/dto/TokenDTO.java
@@ -0,0 +1,10 @@
+package xml.team7.userservice.dto;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+public class TokenDTO {
+    private String token;
+}
---
+++ zuul/src/main/java/xml/team7/zuul/dto/RoleDTO.java
@@ -0,0 +1,15 @@
+package xml.team7.zuul.dto;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.Set;
+
+@Getter @Setter
+@NoArgsConstructor
+public class RoleDTO {
+    private String username;
+    private Set<String> roles;
+    private Set<String> privileges;
+}

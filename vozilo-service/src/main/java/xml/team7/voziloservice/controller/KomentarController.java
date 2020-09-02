a40d1d98a3f53a11a7cd9448f58160054d505142
Katarina Prodanovic <57413828+Katarina1910@users.noreply.github.com>
2020-09-02 18:47:39 +0200

    added comment controller with all necesary methods

---
+++ vozilo-service/src/main/java/xml/team7/voziloservice/controller/KomentarController.java
@@ -0,0 +1,46 @@
+package xml.team7.voziloservice.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import xml.team7.voziloservice.dto.KomentarDTO;
+import xml.team7.voziloservice.service.KomentarService;
+
+@RestController
+@RequestMapping(value = "/api/komentar")
+public class KomentarController {
+
+    @Autowired
+    private KomentarService komentarService;
+
+
+    @PostMapping()
+    private ResponseEntity<?> dodajKomentar(@RequestBody KomentarDTO komentarDTO) {
+        return this.komentarService.kreirajKomentar(komentarDTO, false);
+    }
+
+    @PostMapping(value = "/odgovor")
+    private ResponseEntity<?> dodajKomentarReply(@RequestBody KomentarDTO komentarDTO) {
+        return this.komentarService.kreirajKomentar(komentarDTO, true);
+    }
+
+    @GetMapping(value = "/getKomentareVozilo/{voziloId}")
+    private ResponseEntity<?> pokupiKomentare(@PathVariable Long voziloId){
+        return this.komentarService.pokupiKomentare(voziloId);
+    }
+
+    @GetMapping(value = "/provjeri/{username}/{id}")
+    private ResponseEntity<?> provjerikomentar(@PathVariable Long id, @PathVariable String username){
+        return this.komentarService.provjeriKomentar(username, id);
+    }
+
+    @GetMapping()
+    private ResponseEntity<?> pokupiKomentareSve(){
+        return this.komentarService.pokupiSveKomentare();
+    }
+
+    @GetMapping(value = "/odobri/{flag}/{id}")
+    private ResponseEntity<?> odobriKomentar(@PathVariable Boolean flag, @PathVariable Long id){
+        return this.komentarService.odobriKomentar(flag, id);
+    }
+}

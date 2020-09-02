ceb59c4b76d1e793abd2e54b7a51e1d28ecf202d
Katarina Prodanovic <57413828+Katarina1910@users.noreply.github.com>
2020-09-02 09:25:45 +0200

    added method for pricelist in vozilo

---
+++ vozilo-service/src/main/java/xml/team7/voziloservice/controller/CjenovnikController.java
@@ -0,0 +1,37 @@
+package xml.team7.voziloservice.controller;
+
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import xml.team7.voziloservice.dto.CjenovnikDTO;
+import xml.team7.voziloservice.service.CjenovnikService;
+
+@RestController
+@RequestMapping(value = "/api/cjenovnik")
+public class CjenovnikController {
+
+    @Autowired
+    private CjenovnikService cjenovnikService;
+
+    @GetMapping(value = "/getCjenovnik/{id}")
+    private ResponseEntity<?> getCjenovnik(@PathVariable Long id){
+        return this.cjenovnikService.getCjenovnik(id);
+    }
+
+    @PostMapping("/dodaj")
+    private ResponseEntity<?> dodajCjenovnik(@RequestBody CjenovnikDTO cjenovnikDTO){
+        return this.cjenovnikService.dodajCjenovnik(cjenovnikDTO);
+    }
+
+    @PostMapping ("/izmjeni")
+    private ResponseEntity<?> izmjeniCjenovnik(@RequestBody CjenovnikDTO cjenovnikDTO){
+        return this.cjenovnikService.izmjeniCjenovnik(cjenovnikDTO);
+    }
+
+    @GetMapping(value = "/getCjenovnikUsername/{username}")
+    private ResponseEntity<?> getCjenovnik(@PathVariable String username){
+        return this.cjenovnikService.getCjenovnikUsername(username);
+    }
+
+}

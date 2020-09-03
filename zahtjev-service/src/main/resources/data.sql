bc033db63c7ca2066d037cccedbb73ae16e9a485
Dusan <dusan.dsavic.97@gmail.com>
2020-09-03 13:46:03 +0200

    data.sql changed

--- zahtjev-service/src/main/resources/data.sql
+++ zahtjev-service/src/main/resources/data.sql
@@ -12,10 +12,7 @@ insert into `oglasi` (id, user_id) values (3, 3);
 insert into `zahtjev_za_iznajmljivanje` (id, datum_kreiranja, doo, mjesto_preuzimanja, od, potvrdjen, status, bundle_id, oglas_id, user_id)
 values (1, '2020-08-06 21:58:58', '2020-09-26 21:58:58', 'Novi Sad', '2020-09-11 21:58:58', false, 'PAID', null, 1, 2);
 insert into `zahtjev_za_iznajmljivanje` (id, datum_kreiranja, doo, mjesto_preuzimanja, od, potvrdjen, status, bundle_id, oglas_id, user_id)
-values (2, '2020-08-12 21:58:58', '2020-09-29 21:58:58', 'Novi Sad', '2020-09-20 21:58:58', false, 'PAID', null, 2, 5);
+values (2, '2020-08-12 21:58:58', '2020-09-29 21:58:58', 'Novi Sad', '2020-09-20 21:58:58', false, 'PAID', null, 2, 2);
 insert into `zahtjev_za_iznajmljivanje` (id, datum_kreiranja, doo, mjesto_preuzimanja, od, potvrdjen, status, bundle_id, oglas_id, user_id)
 values (3, '2020-08-12 21:58:58', '2020-09-29 21:58:58', 'Novi Sad', '2020-08-20 21:58:58', false, 'PAID', null, 2, 2);
 insert into `zahtjev_za_iznajmljivanje` (id, datum_kreiranja, doo, mjesto_preuzimanja, od, potvrdjen, status, bundle_id, oglas_id, user_id)
-values (4, '2020-08-12 21:58:58', '2020-08-30 21:58:58', 'Novi Sad', '2020-08-14 21:58:58', false, 'PAID', null, 2, 2);
-insert into `zahtjev_za_iznajmljivanje` (id, datum_kreiranja, doo, mjesto_preuzimanja, od, potvrdjen, status, bundle_id, oglas_id, user_id)
-values (5, '2020-08-12 21:58:58', '2020-08-15 21:58:58', 'Novi Sad', '2020-08-07 21:58:58', false, 'PAID', null, 3, 2);

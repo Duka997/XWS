01f8ed2cb2796bffa0b4e4af713aa2b712b66801
Katarina Prodanovic <57413828+Katarina1910@users.noreply.github.com>
2020-09-04 15:15:32 +0200

    Some changes

--- zahtjev-service/src/main/resources/data.sql
+++ zahtjev-service/src/main/resources/data.sql
@@ -2,11 +2,19 @@
 insert into users (id, username) values (1, 'kaca');
 insert into users (id, username) values (2, 'milica');
 insert into users (id, username) values (3, 'nevena');
+insert into users (id, username) values (4, 'user1');
+insert into users (id, username) values (5, 'user2');
+insert into users (id, username) values (6, 'agent1');
 
 -- ADS
 insert into `oglasi` (id, user_id) values (1, 3);
 insert into `oglasi` (id, user_id) values (2, 3);
 insert into `oglasi` (id, user_id) values (3, 3);
+insert into `oglasi` (id, user_id) values (4, 4);
+insert into `oglasi` (id, user_id) values (5, 4);
+insert into `oglasi` (id, user_id) values (6, 3);
+insert into `oglasi` (id, user_id) values (7, 3);
+
 
 -- RENT REQUESTS
 insert into `zahtjev_za_iznajmljivanje` (id, datum_kreiranja, doo, mjesto_preuzimanja, od, potvrdjen, status, bundle_id, oglas_id, user_id)
